9e22f071df243ca3b527b0da8e1ad2543cee94f9
Joel Gustafson <joelg@mit.edu>
2022-09-08 18:04:10 -0400

    makde build.zig a little more programmable

--- build.zig
+++ build.zig
@@ -11,41 +11,14 @@ pub fn build(b: *std.build.Builder) void {
   // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
   const mode = b.standardReleaseOptions();
 
-  const lmdb = std.build.Pkg{ .name = "lmdb", .path = .{ .path = "lmdb/lib.zig" } };
-  const okra = std.build.Pkg{ .name = "okra", .path = .{ .path = "src/lib.zig" }, .dependencies = &.{ lmdb } };
+  buildArchOS(b, "aarch64-macos");
+  buildArchOS(b, "aarch64-linux-gnu");
+  buildArchOS(b, "aarch64-linux-musl");
+  buildArchOS(b, "x86_64-macos");
+  buildArchOS(b, "x86_64-linux-gnu");
+  buildArchOS(b, "x86_64-linux-musl");
 
-  const lmdbSources: []const []const u8 = &.{
-    "libs/openldap/libraries/liblmdb/mdb.c",
-    "libs/openldap/libraries/liblmdb/midl.c",
-  };
-
-  const cli = b.addExecutable("okra", "cli/main.zig");
-  cli.setTarget(target);
-  cli.setBuildMode(mode);
-  cli.addPackagePath("zig-cli", "libs/zig-cli/src/main.zig");
-  cli.addPackage(lmdb);
-  cli.addPackage(okra);
-  cli.addIncludeDir("libs/openldap/libraries/liblmdb");
-  cli.addCSourceFiles(lmdbSources, &.{ });
-  cli.install();
-
-  const run_cmd = cli.run();
-  run_cmd.step.dependOn(b.getInstallStep());
-  if (b.args) |args| run_cmd.addArgs(args);
-
-  const run_step = b.step("run", "Run the CLI");
-  run_step.dependOn(&run_cmd.step);
-
-  const napi = b.addSharedLibrary("okra", "napi/lib.zig", .unversioned);
-  napi.setTarget(target);
-  napi.setBuildMode(mode);
-  napi.addPackage(okra);
-  napi.addIncludeDir("/usr/local/include/node");
-  napi.addIncludeDir("libs/openldap/libraries/liblmdb");
-  napi.addCSourceFiles(lmdbSources, &.{ });
-  napi.linker_allow_shlib_undefined = true;
-  napi.install();
-
+  // Tests
   const lmdb_tests = b.addTest("lmdb/test.zig");
   lmdb_tests.setTarget(target);
   lmdb_tests.setBuildMode(mode);
@@ -70,4 +43,40 @@ pub fn build(b: *std.build.Builder) void {
   test_step.dependOn(&lmdb_tests.step);
   test_step.dependOn(&okra_tests.step);
   test_step.dependOn(&scanner_tests.step);
+}
+
+const lmdb = std.build.Pkg{ .name = "lmdb", .path = .{ .path = "lmdb/lib.zig" } };
+const okra = std.build.Pkg{ .name = "okra", .path = .{ .path = "src/lib.zig" }, .dependencies = &.{ lmdb } };
+
+const lmdbSources: []const []const u8 = &.{
+  "libs/openldap/libraries/liblmdb/mdb.c",
+  "libs/openldap/libraries/liblmdb/midl.c",
+};
+
+fn buildArchOS(b: *std.build.Builder, comptime archOS: []const u8) void {
+  const mode = b.standardReleaseOptions();
+  const target = std.zig.CrossTarget.parse(.{ .arch_os_abi = archOS }) catch unreachable;
+  const cli = b.addExecutable("okra", "cli/main.zig");
+  cli.setBuildMode(mode);
+  cli.setTarget(target);
+  cli.addPackagePath("zig-cli", "libs/zig-cli/src/main.zig");
+  cli.addPackage(lmdb);
+  cli.addPackage(okra);
+  cli.addIncludeDir("libs/openldap/libraries/liblmdb");
+  cli.addCSourceFiles(lmdbSources, &.{ });
+  cli.setOutputDir("zig-out/" ++ archOS);
+  cli.linkLibC();
+  cli.install();
+
+  // const napi = b.addSharedLibrary("okra", "napi/lib.zig", .unversioned);
+  // napi.setBuildMode(mode);
+  // napi.setTarget(target);
+  // napi.addPackage(okra);
+  // napi.addIncludeDir("/usr/local/include/node");
+  // napi.addIncludeDir("libs/openldap/libraries/liblmdb");
+  // napi.addCSourceFiles(lmdbSources, &.{ });
+  // napi.setOutputDir("zig-out/" ++ archOS);
+  // napi.linkLibC();
+  // napi.linker_allow_shlib_undefined = true;
+  // napi.install();
 }
