538952e32a368ab3b8937931cf97b6c9e00ecb17
Joel Gustafson <joelg@mit.edu>
2023-03-21 20:40:31 -0400

    Update okra-node/index.d.ts

--- okra-node/index.d.ts
+++ okra-node/index.d.ts
@@ -17,12 +17,12 @@ declare module "@canvas-js/okra-node" {
 		/**
 		 * Open a managed read-only transaction
 		 */
-		public read<R>(callback: (txn: ReadOnlyTransaction, ctx: { signal: AbortSignal }) => Promise<R> | R, options?: { dbi?: string }): Promise<R>
+		public read<R>(callback: (txn: ReadOnlyTransaction) => Promise<R> | R, options?: { dbi?: string }): Promise<R>
 
 		/**
 		 * Open a managed read-write transaction
 		 */
-		public write<R>(callback: (txn: ReadWriteTransaction, ctx: { signal: AbortSignal }) => Promise<R> | R, options?: { dbi?: string }): Promise<R>
+		public write<R>(callback: (txn: ReadWriteTransaction) => Promise<R> | R, options?: { dbi?: string }): Promise<R>
 	}
 
 	export interface ReadOnlyTransaction {
