421fcb458dcc4ca1d630712986207dbe697bd085
Joel Gustafson <joelg@mit.edu>
2023-04-17 10:50:44 -0400

    refactor to use an existing idb database instead of managing its own

--- okra-browser/src/tree.ts
+++ okra-browser/src/tree.ts
@@ -1,108 +1,76 @@
-import { openDB, IDBPDatabase, IDBPObjectStore, IDBPTransaction } from "idb"
+import { IDBPDatabase } from "idb"
 
 import debug from "debug"
 
 import { ReadOnlyTransaction, ReadWriteTransaction } from "./transaction.js"
-import { defaultObjectStoreName, getIDRaw, leafAnchorHash } from "./utils.js"
+import { leafAnchorHash } from "./utils.js"
 
-export type Options<T> = {
-  dbs?: string[]
-  getID?: (value: T) => Uint8Array
-  initializeStore?: (dbi: string, store: IDBPObjectStore<unknown, string[], string, "versionchange">) => void
-  upgrade?: (db: IDBPDatabase, oldVersion: number, newVersion: number | null, transaction: IDBPTransaction<unknown, string[], "versionchange">) => void
-}
+export class Tree {
+	private readonly id: string
 
-export class Tree<T = Uint8Array> {
-  public static version = 1
-  public static async open<T = Uint8Array>(name: string, options: Options<T> = {}): Promise<Tree<T>> {
-    const dbs = options.dbs ?? [defaultObjectStoreName]
-    const db = await openDB(name, Tree.version, {
-      upgrade(db, oldVersion, newVersion, transaction) {
-        for (const dbi of dbs) {
-          const store = db.createObjectStore(dbi)
-          store.put({ hash: leafAnchorHash }, [0])
-          if (options.initializeStore) {
-            options.initializeStore(dbi, store)
-          }
-        }
+	public static async open(db: IDBPDatabase, storeName: string): Promise<Tree> {
+		const tree = new Tree(db, storeName)
+		await db.put(storeName, { hash: leafAnchorHash }, [0])
+		return tree
+	}
 
-        if (options.upgrade) {
-          options.upgrade(db, oldVersion, newVersion, transaction)
-        }
-      }
-    })
+	private constructor(
+		public readonly db: IDBPDatabase,
+		public readonly storeName: string
+	) {
+		this.id = `okra:${this.db.name}:${this.storeName}`
+	}
 
-    return new Tree(db, dbs, options.getID ?? getIDRaw)
-  }
+	public async read<R = void>(
+		callback: (txn: ReadOnlyTransaction) => Promise<R> | R
+	): Promise<R> {
+		const log = debug(`${this.id}:read`)
+		log("requesting shared lock")
 
-  private constructor(
-    public readonly db: IDBPDatabase,
-    public readonly dbs: string[],
-    public readonly getID: (value: T) => Uint8Array,
-  ) { }
+		let result: R | undefined = undefined
+		await navigator.locks.request(this.id, { mode: "shared" }, async (lock) => {
+			if (lock === null) {
+				throw new Error(`failed to acquire shared lock ${name}`)
+			}
 
-  public async read<R = void>(callback: (txn: ReadOnlyTransaction<T>) => Promise<R> | R, options: { dbi?: string } = {}): Promise<R> {
-    const dbi = options.dbi ?? defaultObjectStoreName
-    if (!this.dbs.includes(dbi)) {
-      throw new Error("invalid dbi name")
-    }
+			log("acquired shared lock")
+			const txn = new ReadOnlyTransaction(this.db, this.storeName)
+			try {
+				result = await callback(txn)
+			} finally {
+				txn.close()
+				log("releasing shared lock")
+			}
+		})
 
-    const name = `okra:${this.db.name}:${dbi}`
-    const log = debug(`${name}:read`)
-    log("requesting shared lock")
+		return result!
+	}
 
-    let result: R
-    await navigator.locks.request(name, { mode: "shared" }, async (lock) => {
-      if (lock === null) {
-        throw new Error(`failed to acquire shared lock ${name}`)
-      }
+	public async write<R = void>(
+		callback: (txn: ReadWriteTransaction) => Promise<R> | R
+	): Promise<R> {
+		const log = debug(`${this.id}:write`)
 
-      log("acquired shared lock")
-      const txn = new ReadOnlyTransaction<T>(this.db, dbi, this.getID)
-      try {
-        result = await callback(txn)
-      } finally {
-        txn.close()
-        log("releasing shared lock")
-      }
-    })
+		let result: R | undefined = undefined
+		await navigator.locks.request(
+			this.id,
+			{ mode: "exclusive" },
+			async (lock) => {
+				if (lock === null) {
+					throw new Error(`failed to acquire exclusive lock ${name}`)
+				}
 
-    return result!
-  }
+				log("acquired exclusive lock")
+				const txn = new ReadWriteTransaction(this.db, this.storeName)
+				try {
+					result = await callback(txn)
+				} finally {
+					txn.close()
+					log("releasing exclusive lock")
+				}
+			}
+		)
 
-  public async write<R = void>(callback: (txn: ReadWriteTransaction<T>) => Promise<R> | R, options: { dbi?: string } = {}): Promise<R> {
-    const dbi = options.dbi ?? defaultObjectStoreName
-    if (!this.dbs.includes(dbi)) {
-      throw new Error("invalid dbi name")
-    }
-
-    const name = `okra:${this.db.name}:${dbi}`
-    const log = debug(`${name}:write`)
-
-    let result: R
-    await navigator.locks.request(name, { mode: "exclusive" }, async (lock) => {
-      if (lock === null) {
-        throw new Error(`failed to acquire exclusive lock ${name}`)
-      }
-
-      log("acquired exclusive lock")
-      const txn = new ReadWriteTransaction<T>(this.db, dbi, this.getID)
-      try {
-        result = await callback(txn)
-      } finally {
-        txn.close()
-        log("releasing exclusive lock")
-      }
-    })
-
-    return result!
-  }
-
-  public close() {
-    this.db.close()
-  }
-
-  public async reset(): Promise<void> {
-    await Promise.all(this.dbs.map((dbi) => this.write((txn) => txn.reset(), { dbi })))
-  }
+		return result!
+	}
 }
--- okra-browser/src/utils.ts
+++ okra-browser/src/utils.ts
@@ -1,109 +1,98 @@
-import { blake3 } from '@noble/hashes/blake3'
-import { Node } from './schema.js'
-
-export const defaultObjectStoreName = "mst"
-
-export const getIDRaw = <T>(value: T) => {
-  if (value instanceof Uint8Array) {
-    return value
-  } else {
-    throw new TypeError("unexpected value - provide a getID for non-Uint8Array values")
-  }
-}
+import { blake3 } from "@noble/hashes/blake3"
+import { Node } from "./schema.js"
 
 export const K = 16
 export const Q = 32
-
 
 export const leafAnchorHash = blake3(new Uint8Array([]), { dkLen: K })
 
 const limit = Number((1n << 32n) / BigInt(Q))
 
 export function isSplit(hash: Uint8Array): boolean {
-  const view = new DataView(hash.buffer, hash.byteOffset, 4)
-  return view.getUint32(0) < limit
+	const view = new DataView(hash.buffer, hash.byteOffset, 4)
+	return view.getUint32(0) < limit
 }
 
 export function lessThan(a: Uint8Array | null, b: Uint8Array | null): boolean {
-  if (a === null || b === null) {
-    return b !== null
-  }
+	if (a === null || b === null) {
+		return b !== null
+	}
 
-  let x = a.length
-  let y = b.length
+	let x = a.length
+	let y = b.length
 
-  for (let i = 0, len = Math.min(x, y); i < len; ++i) {
-    if (a[i] !== b[i]) {
-      x = a[i]
-      y = b[i]
-      break
-    }
-  }
+	for (let i = 0, len = Math.min(x, y); i < len; ++i) {
+		if (a[i] !== b[i]) {
+			x = a[i]
+			y = b[i]
+			break
+		}
+	}
 
-  return (x < y)
+	return x < y
 }
 
 export const equalArrays = (a: Uint8Array, b: Uint8Array) =>
-  a.length === b.length && a.every((byte, i) => byte === b[i])
+	a.length === b.length && a.every((byte, i) => byte === b[i])
 
 export function equalKeys(a: Uint8Array | null, b: Uint8Array | null): boolean {
-  if (a === null || b === null) {
-    return a === null && b === null
-  } else {
-    return equalArrays(a, b)
-  }
+	if (a === null || b === null) {
+		return a === null && b === null
+	} else {
+		return equalArrays(a, b)
+	}
 }
 
-export const equalNodes = <T>(a: Node<T>, b: Node<T>) => a.level === b.level && equalKeys(a.key, b.key) && equalArrays(a.hash, b.hash)
+export const equalNodes = <T>(a: Node, b: Node) =>
+	a.level === b.level && equalKeys(a.key, b.key) && equalArrays(a.hash, b.hash)
 
 const size = new ArrayBuffer(4)
 const view = new DataView(size)
 
 export function hashEntry(key: Uint8Array, value: Uint8Array): Uint8Array {
-  const hash = blake3.create({ dkLen: K })
-  view.setUint32(0, key.length)
-  hash.update(new Uint8Array(size))
-  hash.update(key)
-  view.setUint32(0, value.length)
-  hash.update(new Uint8Array(size))
-  hash.update(value)
-  return hash.digest()
+	const hash = blake3.create({ dkLen: K })
+	view.setUint32(0, key.length)
+	hash.update(new Uint8Array(size))
+	hash.update(key)
+	view.setUint32(0, value.length)
+	hash.update(new Uint8Array(size))
+	hash.update(value)
+	return hash.digest()
 }
 
 const byteToHex = new Array<string>(0xff)
 
 for (let n = 0; n <= 0xff; ++n) {
-  byteToHex[n] = n.toString(16).padStart(2, "0")
+	byteToHex[n] = n.toString(16).padStart(2, "0")
 }
 
 export function toHex(value: Uint8Array) {
-  const octects = new Array<string>(value.length)
-  for (let i = 0; i < value.length; i++) {
-    octects[i] = byteToHex[value[i]]
-  }
+	const octects = new Array<string>(value.length)
+	for (let i = 0; i < value.length; i++) {
+		octects[i] = byteToHex[value[i]]
+	}
 
-  return octects.join("")
+	return octects.join("")
 }
 
 export function fromHex(value: string): Uint8Array {
-  if (value.length % 2 !== 0) {
-    throw new Error("invalid hex string")
-  }
+	if (value.length % 2 !== 0) {
+		throw new Error("invalid hex string")
+	}
 
-  const array = new Uint8Array(value.length / 2)
-  for (let i = 0; i < value.length; i += 2) {
-    array[i / 2] = parseInt(value.slice(i, i + 2), 16)
-  }
+	const array = new Uint8Array(value.length / 2)
+	for (let i = 0; i < value.length; i += 2) {
+		array[i / 2] = parseInt(value.slice(i, i + 2), 16)
+	}
 
-  return array
+	return array
 }
 
-
 export function shuffle<T>(array: T[]) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    const temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-  }
-} 
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		const temp = array[i]
+		array[i] = array[j]
+		array[j] = temp
+	}
+}
