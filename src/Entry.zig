9c2140d37e21e45c5bbeca9e7b47c340791d2405
Joel Gustafson <joelg@mit.edu>
2024-01-18 18:42:24 -0500

    remove unnecessary cast

--- src/Entry.zig
+++ src/Entry.zig
@@ -8,10 +8,10 @@ value: []const u8,
 pub fn hash(key: []const u8, value: []const u8, result: []u8) void {
     var digest = std.crypto.hash.Blake3.init(.{});
     var size: [4]u8 = undefined;
-    std.mem.writeInt(u32, &size, @as(u32, @intCast(key.len)), .big);
+    std.mem.writeInt(u32, &size, @intCast(key.len), .big);
     digest.update(&size);
     digest.update(key);
-    std.mem.writeInt(u32, &size, @as(u32, @intCast(value.len)), .big);
+    std.mem.writeInt(u32, &size, @intCast(value.len), .big);
     digest.update(&size);
     digest.update(value);
     digest.final(result);
